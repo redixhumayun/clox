21bd6c7cac38fe8d9e25e1d7be6a65d4097ee2d3
Zaid Humayun <redixhumayun@gmail.com>
2022-08-04 02:09:57 +0530

    have a complete scanner

--- debug.c
+++ debug.c
@@ -2,14 +2,6 @@
 
 #include "debug.h"
 
-void disassembleChunk(Chunk* chunk, const char* name) {
-  printf("== %s ==\n", name);
-
-  for (int offset = 0; offset < chunk->count;) {
-    offset = disassembleInstruction(chunk, offset);
-  }
-}
-
 static int simpleInstruction(const char* name, int offset) {
   printf("%s\n", name);
   return offset + 1;
@@ -36,10 +28,28 @@ int disassembleInstruction(Chunk* chunk, int offset) {
   switch (instruction) {
     case OP_CONSTANT:
       return constantInstruction("OP_CONSTANT", chunk, offset);
+    case OP_NEGATE:
+      return simpleInstruction("OP_NEGATE", offset);
     case OP_RETURN:
       return simpleInstruction("OP_RETURN", offset);
+     case OP_ADD:
+      return simpleInstruction("OP_ADD", offset);
+    case OP_SUBTRACT:
+      return simpleInstruction("OP_SUBTRACT", offset);
+    case OP_MULTIPLY:
+      return simpleInstruction("OP_MULTIPLY", offset);
+    case OP_DIVIDE:
+      return simpleInstruction("OP_DIVIDE", offset);
     default:
       printf("Unknown opcode %d\n", instruction);
       return offset + 1;
+  }
+}
+
+void disassembleChunk(Chunk* chunk, const char* name) {
+  printf("== %s ==\n", name);
+
+  for (int offset = 0; offset < chunk->count;) {
+    offset = disassembleInstruction(chunk, offset);
   }
 }
--- main.c
+++ main.c
@@ -1,17 +1,73 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #include "common.h"
 #include "chunk.h"
 #include "debug.h"
+#include "vm.h"
 
 int main(int argc, const char* argv[]) {
-  Chunk chunk;
-  initChunk(&chunk);
-  int constant = addConstant(&chunk, 1.2);
-  writeChunk(&chunk, OP_CONSTANT, 123);
-  writeChunk(&chunk, constant, 123);
-  writeChunk(&chunk, OP_RETURN, 123);
-  disassembleChunk(&chunk, "test chunk");
-  freeChunk(&chunk);
+  initVM();
+
+  if (argc == 1) {
+    repl();
+  } else if (argc == 2) {
+    runFile(argv[1]);
+  } else {
+    fprintf(stderr, "Usage: clox [PATH]");
+    exit(64);
+  }
+
+  freeVM();
   return 0;
+}
+
+void repl() {
+  char line[1024];
+  for(;;) {
+    printf("> ");
+    if (fgets(line, sizeof(line), stdin) == NULL) {
+      printf("Error");
+      break;
+    } 
+
+    interpret(line);
+  }
+}
+
+void runFile(const char* path) {
+  char* source = readFile(path);
+  InterpretResult result = interpret(source);
+  free(source);
+
+  if (result == INTERPRET_COMPILE_ERROR) exit(65);
+  if (result == INTERPRET_RUNTIME_ERROR) exit(70);
+}
+
+static char* readFile(const char* path) {
+  FILE* file = fopen(path, "rb");
+  if (file == NULL) {
+    fprintf(stderr, "Could not open file \"%s\".\n", path);
+    exit(74);
+  }
+
+  fseek(file, 0L, SEEK_END);
+  size_t fileSize = ftell(file);
+  rewind(file);
+
+  char* buffer = (char*)malloc(fileSize + 1);
+  if (buffer == NULL) {
+    fprintf(stderr, "Not enough memory to read \"%s\".\n", path);
+    exit(74);
+  }
+  size_t bytesRead = fread(buffer, sizeof(char), fileSize, file);
+  if (bytesRead < fileSize) {
+    fprintf(stderr, "Could not read the entire file \"%s\".\n", path);
+    exit(74);
+  }
+  buffer[bytesRead] = '\0';
+
+  fclose(file);
+  return buffer;
 }
