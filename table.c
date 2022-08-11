afb8de40f0035d8527d2403c7757d9840e11628d
Zaid Humayun <redixhumayun@gmail.com>
2022-08-12 01:12:47 +0530

    fix to return object string

--- table.c
+++ table.c
@@ -121,7 +121,7 @@ ObjString* tableFindString(Table* table, const char* chars, int length, uint32_t
     } else if (entry->key->length == length && 
                 entry->key->hash == hash && 
                 memcmp(entry->key->chars, chars, length) == 0) {
-                  return entry;
+                  return entry->key;
                 }
     index = (index + 1) % table->capacity;
   }
